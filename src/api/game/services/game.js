f4ea211638c53292c626d5fd5584a1e30f79607e
Jhonata Martins da Costa <jhomcosta@gmail.com>
2022-05-16 00:15:04 -0300

    scrapping de dados ok

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -6,10 +6,30 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
+const axios = require("axios");
+
+async function getGameInfo(slug) {
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom
+  const body = await axios.get(`https://www.gog.com/en/game/${slug}`)
+  const dom = new JSDOM(body.data)
+
+  const description = dom.window.document.querySelector(".description")
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+
+  }
+}
+
 module.exports = createCoreService('api::game.game', ({ strapi }) => ({
   async populate(params) {
-    const category = await strapi.service('api::category.category').find({ name: "Action" })
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&search=Witcher`
 
-    console.log(category)
+    const { data: { products } } = await axios.get(gogApiUrl)
+
+    console.log(await getGameInfo(products[1].slug))
   }
 }));
